1bfafcefaeadce48ec4338f8b37f82aa87f8e495
Andriy Kostenko <a.kostenkouk@gmail.com>
2025-12-13 19:47:36 -0700

    upd /report endpoint with proper response model

--- backend/routes/report_routes.py
+++ backend/routes/report_routes.py
@@ -2,21 +2,28 @@ from fastapi import APIRouter, UploadFile, File, HTTPException, status
 
 from schemas.report_shcemas import DetectionRequestSchema
 from pdf_report_generator import report_generator
+from schemas.report_shcemas import ReportResponseSchema
+
 
 report_router = APIRouter()
+
 
 # TODO: Create a service to generate PDF report using PDFReportGenerator
 @report_router.post("/report",
                      summary="Generate PDF report from detections and annotated image",
                      response_description="PDF report generation status",
-                     status_code=status.HTTP_201_CREATED)
-def generate_the_report(data: DetectionRequestSchema):  
+                     status_code=status.HTTP_201_CREATED,
+                     response_model=ReportResponseSchema)
+async def generate_the_report(data: DetectionRequestSchema):  
     try:
         pdf_path = report_generator.generate_report(
             detections=data.detections,
             annotated_image_base64=data.annotated_image
         )
-        return {"message": "PDF report generated successfully", "pdf_path": pdf_path}
+        return ReportResponseSchema(
+            message="PDF report generated successfully",
+            report_path=str(pdf_path)
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating PDF report: {e}")
         
